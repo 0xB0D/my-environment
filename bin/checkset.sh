a5ce43afafe39715b7ecbb474a117133f363d6c3
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2022-05-19 12:48:23 +0100

    bin/checkset.sh: Use a default path for KERNELPATH if not set
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- bin/checkset.sh
+++ bin/checkset.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if [ -z "$KERNELPATH" ]
+then
+	echo "Setting KERNELPATH to this directory which is probably what you want"
+	echo "Please set KERNELPATH when running this utility to over-ride";
+	echo "KERNELPATH=`pwd` ~/my-environment/bin/checkset.sh"
+	KERNELPATH=`pwd`
+fi
 for file in *.patch;
-do /home/deckard/Development/linux/scripts/checkpatch.pl --strict $file;
+do $KERNELPATH/scripts/checkpatch.pl --strict $file;
 done
